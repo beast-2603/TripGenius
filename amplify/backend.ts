f83536c4c5986add9fd4972b97dd13d78996a36e
beast-2603 <senapati.sohan31@gmail.com>
2025-04-19 07:12:35 +0530

    none

--- amplify/backend.ts
+++
@@ -1,9 +0,0 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { sayHello } from './functions/say-hello/resource';
-
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
-  sayHello
-});
